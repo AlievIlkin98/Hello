e0816877deeec8ed73e7af0d5cf8060430890dba
AlievIlkin98 <109948880+AlievIlkin98@users.noreply.github.com>
2022-08-19 14:17:43 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+
+
+
+
+
+    }
+}
